900ba9ada9d632d761a13514135b1b06dd55586f
Rishikesh chava <102192822+Rishikesh5198@users.noreply.github.com>
2022-04-12 19:00:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+function click1()
+{ 
+    
+
+    let a=document.getElementsByClassName("answer");
+     a[0].style.display="flex";
+
+   let a1=document.getElementsByClassName("minusign1");
+   a1[0].style.display="block";
+
+   let a2=document.getElementsByClassName("plusign1");
+   a2[0].style.display="none";
+
+}
+
+function click2()
+{
+    let a=document.getElementsByClassName("answer");
+     a[0].style.display="none";
+
+     let a1=document.getElementsByClassName("minusign1");
+     a1[0].style.display="none";
+
+   let a2=document.getElementsByClassName("plusign1");
+   a2[0].style.display="block";
+}
+
+function click3()
+{
+
+    let a=document.getElementsByClassName("answer");
+     a[1].style.display="flex";
+
+   let a1=document.getElementsByClassName("minusign1");
+   a1[1].style.display="block";
+
+   let a2=document.getElementsByClassName("plusign1");
+   a2[1].style.display="none";
+}
+function click4()
+{
+    let a=document.getElementsByClassName("answer");
+     a[1].style.display="none";
+
+     let a1=document.getElementsByClassName("minusign1");
+     a1[1].style.display="none";
+
+   let a2=document.getElementsByClassName("plusign1");
+   a2[1].style.display="block";
+
+}
+
+function click5()
+{
+
+    let a=document.getElementsByClassName("answer");
+     a[2].style.display="flex";
+
+   let a1=document.getElementsByClassName("minusign1");
+   a1[2].style.display="block";
+
+   let a2=document.getElementsByClassName("plusign1");
+   a2[2].style.display="none";
+}
+function click6()
+{
+    let a=document.getElementsByClassName("answer");
+     a[2].style.display="none";
+
+     let a1=document.getElementsByClassName("minusign1");
+     a1[2].style.display="none";
+
+   let a2=document.getElementsByClassName("plusign1");
+   a2[2].style.display="block";
+
+}
